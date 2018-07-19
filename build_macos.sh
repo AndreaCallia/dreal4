ca7a27e32ea79b7fbb44da8dbe984152a86e2ca7
Andrea Callia <ac4014@casca.doc.ic.ac.uk>
2018-07-19 15:29:00 +0100

    Improved building scripts.

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm dreal/solver/sat_solver.cc
+[[ "$?" != 0 ]] && { >&2 echo "Failed removing link to sat_solver.cc" ; exit 8 ; }
+ln -s `pwd`/smts/sat_solver.cc dreal/solver/sat_solver.cc
+[[ "$?" != 0 ]] && { >&2 echo "Failed linking patched sat_solver.cc" ; exit 9 ; }
 bazel build //...
 [[ "$?" != 0 ]] && { >&2 echo "Failed build." ; exit 1 ; }
 bazel build //:archive
