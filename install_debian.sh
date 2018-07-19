0a1cfc13158604bfdb9dc403de1dc39379820807
Andrea Callia <ac4014@casca.doc.ic.ac.uk>
2018-07-19 12:15:43 +0100

    Added installation script for Debian.

---
+++ install_debian.sh
@@ -0,0 +1,11 @@
+bazel build //...
+[[ "$?" != 0 ]] && { >&2 echo "Failed build." ; exit 1 ; }
+bazel build //:package_debian
+[[ "$?" != 0 ]] && { >&2 echo "Failed generation of debian package." ; exit 2 ; }
+dpkg -i bazel-bin/dreal_4.18.05.2_amd64.deb
+[[ "$?" != 0 ]] && { >&2 echo "Failed installation of debian package." ; exit 3 ; }
+cp dreal/solver/sat_solver.h /opt/dreal/4.18.05.2/include/dreal/solver/
+[[ "$?" != 0 ]] && { >&2 echo "Failed installation of extra files." ; exit 4 ; }
+cp /data/projects/dreal4/bazel-dreal4/external/picosat/picosat.h /opt/dreal/4.18.05.2/include/dreal/solver/
+[[ "$?" != 0 ]] && { >&2 echo "Failed installation of extra files." ; exit 4 ; }
+echo "Installation completed."
